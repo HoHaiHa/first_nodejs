931b73fba228a78d078b54647f63b0c55da367d7
HoHaiHa <hohaiha0210@gmail.com>
2023-09-19 11:30:41 +0700

    final update

---
+++ src/app/controllers/CourseController.js
@@ -0,0 +1,58 @@
+const Course = require('../models/Course');
+const {multipleMongooseToObject, mongooseToObject} = require('../../util/mongoose')
+
+class CourseController {
+    //[get] /courses/:slug
+    show(req, res, next) {
+
+        Course.findOne({slug: req.params.slug})
+            .then(course => {
+                res.render('courses/show',{course: mongooseToObject(course)})
+            })
+            .catch(next)
+    }
+
+    //[get] /courses/create
+    create(req, res, next) {
+        res.render('courses/create')
+    }
+
+    //[post] /courses/store
+    store(req, res, next) {
+        const formData = req.body
+        formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`
+        const course = new Course(formData)
+        course.save()
+           .then(() => res.redirect('/'))
+           .catch(error => {
+            
+           })
+    }
+
+    //[get] /courses/:id/edit
+    edit(req, res, next) {
+        Course.findById(req.params.id)
+            .then(course => res.render('courses/edit',{
+                course: mongooseToObject(course)
+            }))
+            .catch(next)
+    }
+
+    //[put] /courses/:id
+    update(req, res, next) {
+        Course.updateOne({_id: req.params.id}, req.body)
+            .then(() => res.redirect('/me/stored/courses'))
+            .catch(next)
+    }
+
+    //[del] /courses/:id
+    delete(req, res, next){
+        Course.deleteOne({_id: req.params.id})
+            .then(() => res.redirect('back'))
+            .catch(next)
+    }
+    
+
+}
+
+module.exports = new CourseController();
--- src/index.js
+++ src/index.js
@@ -4,8 +4,17 @@ const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
+const methodOverride = require('method-override')
+
+
 
 const route = require('./routes');
+const db = require('./config/db');
+
+
+
+//connect db
+db.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -16,6 +25,8 @@ app.use(
 );
 app.use(express.json());
 
+app.use(methodOverride('_method'))
+
 // http logger
 //app.use(morgan('combined'))
 
@@ -24,14 +35,23 @@ app.engine(
     'hbs',
     handlebars.engine({
         extname: '.hbs',
+        helpers:{
+            sum: (a,b) => a + b,
+        }
     }),
+    
 );
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
 //routes init
-route(app);
+
 
 app.listen(port, () => {
-    console.log(`Example app listening on port http://localhost:${port}`);
+    console.log(`App listening on port http://localhost:${port}`);
 });
+
+route(app);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
